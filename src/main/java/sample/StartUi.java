b97244e6f81d21f3b4875c55a2c5b7aef2db4a6d
Connor Mahon <connor.mahon@ontariotehcu.net>
2020-03-25 16:29:20 -0400

    Fixed up gradle

--- src/main/java/sample/StartUi.java
+++ src/main/java/sample/StartUi.java
@@ -3,7 +3,6 @@ package main.java.sample;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
 public class StartUi extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
